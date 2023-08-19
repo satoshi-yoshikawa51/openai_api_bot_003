0cecb90dba94c756fc18e743794974b16dd8f7e2
satoshi-yoshikawa51 <142549863+satoshi-yoshikawa51@users.noreply.github.com>
2023-08-20 08:21:10 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -70,9 +70,6 @@ st.markdown(custom_css, unsafe_allow_html=True)
 st.image("company_policy.gif")
 st.markdown("<p style='font-size:14px;'>知りたい会社のルールに関する質問を入力してください。<br>尚、現在は「PC利用に関するルール」「交通費に関するルール」「経費に関するルール」しか学習していない為、それ以外には答えられません。<br>※このルールは一般のIT会社を想定したAIが作ったダミーのルールです。</p>", unsafe_allow_html=True)
 
-
-user_input = st.text_input( key="user_input", on_change=communicate)
-
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
